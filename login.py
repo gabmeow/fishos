5fa0e947ec3f0e33ce0e2d6f316c8b4fa1c9303e
Sleepy <76005806+bigcat11262@users.noreply.github.com>
2021-10-17 19:51:06 +0200

    Update login.py

--- login.py
+++ login.py
@@ -21,17 +21,17 @@ if setup == "1":
     name = input(str("Please Enter Your Username: "))
     pas = input(str("Please Enter Your Password: "))
 
-    with open('user/username.txt', 'w') as f:
+    with open('username.txt', 'w') as f:
         f.writelines(name)
 
-    with open('user/password.txt', 'w') as f:
+    with open('password.txt', 'w') as f:
         f.writelines(pas)
     print("Setup Finished!")
     print("Please Press Enter To Restart The OS")
 
 if setup == '2':
-    login_pass = open('user/password.txt')
-    login_name = open('user/username.txt')
+    login_pass = open('password.txt')
+    login_name = open('username.txt')
     lp = login_pass.read()
     ln = login_name.read()
 
