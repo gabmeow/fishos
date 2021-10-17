d2274eb6b56a8be1a446bc8ad34d6a76dcd60c72
Sleepy <76005806+bigcat11262@users.noreply.github.com>
2021-10-17 19:47:22 +0200

    Create explorer.py

---
+++ explorer.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+
+def openFile():
+    filepath = filedialog.askopenfilename(initialdir="/",
+                                            title="Open Files",
+                                            filetypes= (("text files", "*.txt"),
+                                            ("all files", "*.*")))
+    os.startfile(filepath)
+window = Tk()
+button = Button(text="Open",command=openFile)
+button.pack()
+window.mainloop()
